31d4d5a246282eeb58897bc9712d8fd735ede2ee
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-05-01 05:59:41 +0200

    convert JsonReportParser to kotlin

---
+++ oclint/src/main/java/me/raatiniemi/sonar/oclint/report/JsonReportParser.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 Tobias Raatiniemi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.sonar.oclint.report
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import me.raatiniemi.sonar.oclint.Violation
+import org.sonar.api.utils.log.Loggers
+import java.io.File
+import java.io.IOException
+import java.util.*
+
+internal class JsonReportParser(private val objectMapper: ObjectMapper) : ViolationReportParser {
+    override fun parse(reportFile: File): Optional<List<Violation>> {
+        return try {
+            val report = objectMapper.readValue(reportFile, JsonReport::class.java)
+            val violations = report.violations.map { transformToViolation(it) }
+
+            Optional.of(violations)
+        } catch (e: IOException) {
+            LOGGER.warn("Unable to parse report file: ${reportFile.path}")
+            Optional.empty()
+        }
+    }
+
+    private fun transformToViolation(violation: JsonReport.Violation) = Violation.builder()
+        .setPath(violation.path)
+        .setStartLine(readStartLine(violation))
+        .setRule(violation.rule)
+        .setMessage(violation.message)
+        .build()
+
+    private fun readStartLine(violation: JsonReport.Violation): Int = with(violation) {
+        if (startLine == 0) {
+            LOGGER.warn("Found empty start line in report for path: {}", path)
+            return 1
+        }
+
+        return startLine
+    }
+
+    companion object {
+        private val LOGGER = Loggers.get(JsonReportParser::class.java)
+    }
+}
